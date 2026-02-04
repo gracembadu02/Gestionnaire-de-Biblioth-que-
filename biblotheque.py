37e3220f09b5814c26f78edb07d6a1442161ac08
Grace <gracembadu02@gmail.com>
2026-02-04 01:22:39 -0500

    feat : Ajouter fonctionnalité Emprunt et Retourne

--- biblotheque.py
+++ biblotheque.py
@@ -7,15 +7,38 @@ Pas besoin d’indiquer les types (String)
 push_back() → append()
 
 Les {} deviennent une indentation
+
+studentFile = dossierEtudiant
+to  borrow = emprunter
+loanDate = dateEmprunt
 """
 
 
-Livres[]
+books=[]
+def addBook(author,book):
 
-def ajouterLivre(auteur,livre):
-
-   Livres.append({
-      "auteur" : auteur;
-       "titre" : livre; 
+   books.append({
+      "autor" : author,
+       "book title" : book,
    })
 
+StudentFile= []
+def borrowBook(loanDate,dateFin,studentName,adress,book):
+   StudentFile.append(
+      {
+         "loanDate" : loanDate,
+         "date fin" : dateFin,
+         "Name of student": studentName,
+         "adress" : adress,
+         "book": book,
+         
+      }
+   )
+   return f"book '{book}'emprunté par {studentName} avec succés."
+
+
+def returnBook(studentName,book):
+   return f"the book '{book}' est retourné par {studentName}."
+
+message = returnBook("Alice Martin", "Python pour débutants")
+print(message)  
